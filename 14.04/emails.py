8fd5dc66c336eea5ad324964c00995049be3ab05
AdamPrusz <79059286+AdamPrusz@users.noreply.github.com>
2021-05-12 00:06:48 +0200

    Add files via upload

---
+++ 14.04/emails.py
@@ -0,0 +1,11 @@
+def check_email(list, my_email):
+    for email in list:
+        if email == list:
+            True
+    return False
+
+
+my_emial = 1
+list = [2,3,4,5]
+
+print(check_email(list,my_emial))
---
+++ 14.04/graf.py
@@ -0,0 +1,16 @@
+graph= {
+    "dom": ["szkoła", "kościół", "bar"]
+    "szkoła": ["dom", "szpital"]
+    "szpital": ["bar", "szkoła"]
+    "bar": ["kościół", "dom", "szpital"]
+    "kościół": ["dom", "bar", ]
+}
+
+
+
+= {
+  0 : [1, 2, 3]
+  1 : [0, 2]
+  2 : [0, 1, 3]
+  3 : [0, 2]
+}
